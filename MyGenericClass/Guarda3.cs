10083ab46bd7e4ca37f3ec13700f512803e6d844
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-31 17:27:47 +0100

    Get item

--- MyGenericClass/Guarda3.cs
+++ MyGenericClass/Guarda3.cs
@@ -6,9 +6,25 @@ namespace MyGenericClass
     {
         private T[] items;
 
+        /// <summary>
+        /// Constructorish
+        /// </summary>
         public Guarda3()
         {
             items = new T[3];
+        }
+
+        /// <summary>
+        /// Returns items
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public T GetItem(int i)
+        {
+            if (i < 0 || i > 2)
+                throw new IndexOutOfRangeException();
+
+            return items[i];
         }
     }
 }
