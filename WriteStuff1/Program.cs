1a8010ba8cc4be1879afc4d141f09e63e7b8d0b5
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-06-01 15:02:44 +0100

    filename = arguments

--- WriteStuff1/Program.cs
+++ WriteStuff1/Program.cs
@@ -11,6 +11,10 @@ namespace WriteStuff1
                 Console.WriteLine("Put in the name of the file.");
                 return;
             }
+
+            //name of the file chosen
+            string fileName = args[0];
+            Queue<string> stringsQueue = new Queue<string>();
         }
     }
 }
