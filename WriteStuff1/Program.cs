f18933e1db9049cf88eaabd2ac82dc601b20d470
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-06-01 15:01:46 +0100

    If args.length <1

---
+++ WriteStuff1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WriteStuff1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Put in the name of the file.");
+                return;
+            }
+        }
+    }
+}
