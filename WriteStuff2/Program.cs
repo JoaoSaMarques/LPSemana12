058ff28eca394c8f94cd27f7e74da5a518a2cb82
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-06-02 10:13:48 +0100

    Removing unecessary parts

--- WriteStuff2/Program.cs
+++ WriteStuff2/Program.cs
@@ -14,21 +14,7 @@ namespace WriteStuff2
 
             //name of the file chosen
             string fileName = args[0];
-            Queue<string> stringsQueue = new Queue<string>();
-
-            while (true)
-            {
-                //Reads user input every sentence
-                Console.Write("Insert a  string: ");
-                string input = Console.ReadLine();
-
-                //If there is nothing, stop program
-                if (string.IsNullOrEmpty(input))
-                break;
-
-                stringsQueue.Enqueue(input);
-            }
-
+            
             //Saves the strings into the file 
             try
             {   
