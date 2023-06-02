6243e48fd9d248f15ac379ee1c9abc97a513ebab
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-06-02 10:10:37 +0100

    Copied first one

---
+++ WriteStuff2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace WriteStuff2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Put in the name of the file.");
+                return;
+            }
+
+            //name of the file chosen
+            string fileName = args[0];
+            Queue<string> stringsQueue = new Queue<string>();
+
+            while (true)
+            {
+                //Reads user input every sentence
+                Console.Write("Insert a  string: ");
+                string input = Console.ReadLine();
+
+                //If there is nothing, stop program
+                if (string.IsNullOrEmpty(input))
+                break;
+
+                stringsQueue.Enqueue(input);
+            }
+
+            //Saves the strings into the file 
+            try
+            {   
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    while (stringsQueue.Count > 0)
+                    {
+                        string str = stringsQueue.Dequeue();
+                        writer.WriteLine(str);
+                    }
+                }
+            }
+
+            Console.WriteLine("Saved the  strings!");
+            //If it does not save
+            catch (IOException e)
+            {
+                Console.WriteLine("Failed to save: " + e.Message);
+            }
+        }
+    }
+}
